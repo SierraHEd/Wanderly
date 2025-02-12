db4c9eb54ec84985fec9651bb4113c615c9ae276
SierraHEd <sierraedwards123@yahoo.com>
2025-02-12 12:36:10 -0500

    Fixed SDK and will now run

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.csc490group3"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.csc490group3"
