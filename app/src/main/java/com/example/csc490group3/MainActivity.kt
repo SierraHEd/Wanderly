dbd79c11f3138ff0bfff941644fc1a64287844bd
Emily Grega <emily1jean@gmail.com>
2025-02-20 22:53:58 -0500

    Added navigation Added dropdown menus where applicable Adjusted layout Worked on logic functionality regarding variables Need Sanitization

--- app/src/main/java/com/example/csc490group3/MainActivity.kt
+++ app/src/main/java/com/example/csc490group3/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.csc490group3
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Button
