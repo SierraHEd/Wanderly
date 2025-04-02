7527195efcd6e93247e0b10cb28c16dd9353bf0f
William Carson <carsw@farmingdale.edu>
2025-04-01 23:47:45 -0400

    please work

--- app/src/main/java/com/example/csc490group3/HomeScreen.kt
+++ app/src/main/java/com/example/csc490group3/HomeScreen.kt
@@ -202,7 +202,6 @@ fun HomeScreen(navController: NavController, viewModel: HomeScreenViewModel = vi
                                     onEditEvent = {}
                                 )
                             }
-
                     }
                 }
             }
